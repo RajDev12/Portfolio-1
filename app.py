aba6f68d5ce891176b6f37da00650729777ef2ec
RajDev12 <rjnath98@gmail.com>
2025-03-26 00:58:21 +0530

    set up

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+# import pages.ContactMe
+from pages import home
+
+# Set page configuration
+st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+
+
+from streamlit_option_menu import option_menu
+st.header("Data Science Portfolio")
+
+selected_menu=option_menu("",
+            ["Home","About","Project","Contact Me"]
+            , orientation="horizontal",
+            icons=[ ],
+            default_index=0)
+col1,col2,col3= st.columns([1,0.3,1])
+if selected_menu=="Home":
+    home.app()
+
+
+# if selected_menu=="About":
+#     pages.About
+
+# if selected_menu=="Project":
+#    pages.Project
+
+# if selected_menu=="Contact Me":
+#     pages.ContactMe
+
+
+
+
+
+
---
+++ pages/home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+def app():
+    col1,col2,col3= st.columns([1,0.3,1])
+    with col1:
+        st.header("Dev Data Science")
+        st.subheader("I don’t know much about you yet! But I’d love to learn.Tell me a little about yourself—your interests, what you do, or anything else you'd like me to remember.😊")
+    with col3:
+        st.image('./assets/gayetri.jpg',width=400)
+
