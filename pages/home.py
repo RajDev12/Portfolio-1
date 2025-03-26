a95e8079471fbb922f5d1a88e9c7358c5ad3b9e8
Raju Devnath <122223306+RajDev12@users.noreply.github.com>
2025-03-26 11:18:29 +0530

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -6,34 +6,4 @@ def app():
         st.subheader("I don’t know much about you yet! But I’d love to learn.Tell me a little about yourself—your interests, what you do, or anything else you'd like me to remember.😊")
     with col3:
         st.image('./assets/images.jpg')
-        st.markdown('''
-**My sexy office co-worker began to stiffen up again. That sprang life into my cock again. A naked Komal was more powerful than any aphrodisiac I could have ever taken. I flipped her over and held her by her divine waist. My hands almost encircled her entire waist.
-
-At that moment, I realized how truly intoxicating a narrow waist was and how Komal felt like a fucktoy who existed solely for my pleasure. My dick knew where it wanted to go. It pushed a little against her tight little asshole.
-
-“Nahin, nahin, nahin! Please nahin! I have never done it before. Main nahin le paungi tera lund gaand mein,” she pleaded. (No, no, no! Please no! I won’t be able to take your dick in my ass.)
-
-I spanked her very hard across her right butt cheek. A red handprint developed on her smooth, white complexion. I told her, “Bandh kar tera randi rona! Tujhe toh main gaand mein hi chodne waala hoon, saali rakheil!” (Stop bitching! I am going to fuck you in the ass, you slut.)
-
-She let out a whimper as I pushed my cock right into her ass. My rod was expanding and stretching this whore’s tight asshole with every inch. I was about halfway into my sexiest office partner’s asshole. She threw her head back in ecstasy, tears running down her face, ruining her mascara.
-
-Her ass getting stuffed with my monster must have brought her into new throes of pleasure because she started screaming, “Chod behenchod, chod!” (Fuck me!)
-
-I didn’t waste a single second and began pounding away into her asshole. Shoving my full length into that tiny passage, I couldn’t believe how tight and amazing she felt. I grabbed her waist and just started fucking her with raw abandon. My dick was thrusting in and out of her ass like a piston.
-
-Her head was tilted back in full enjoyment. I found out then just how flexible my little slut was. I pulled her by the neck towards me, head still tilting back, her back arched. We made out almost upside down, and all the while, I continued to fuck her ass.
-
-She was moaning as we kissed. I’m sure it was drowning out screams, but the position we were in was insane. My hand found its way to her clit. I began to tap on it lightly and rub it roughly. She broke the kiss and lurched ahead, her eyes wide with shock.
-
-She just grabbed my hand and made me rub her clit even faster. The combined pace of us rubbing her clit and me fucking her ass was too much for her. Komal shrieked. Her whole body quivered as another orgasm passed her body. She kept twitching before she passed out and fell – face down, ass up on the bed.
-
-I slapped her awake, pulled her up by her boobs that I still hadn’t enjoyed. I continued fucking her in the ass. My left hand was mauling her soft little left tit. My right hand was spanking her bare ass, making it redder every second. My dick kept pounding right into her brown ring.
-
-I kept this up until Komal screamed again, her body bucking like a wild animal. I could feel myself getting close again. I held onto both of her pillowy soft tits. Squeezing them completely in the palm of my hand. With a final burst of jackhammering my cock into her ass, I climaxed again.
-
-We both shouted as our orgasms hit us at the same moment. I deposited whatever cum I had left into her ass. Her juices spouted out of her onto the bed, and the rest ran down her thighs.
-
-We finally collapsed, my cock still buried in her ass. When it had gone fully limp, I could take it out of her. We lay next to each other, completely spent and well fucked.
-
-That’s all for now! If you liked this story, please write or give me feedback at shatter.star3424@gmail.com. And, oh, wait for part two!**
-''')
+       
